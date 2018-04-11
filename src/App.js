b0e08f9a25f5caf06b65d07449b4db61ca3f9c15
Allen Wong <allencwwong@gmail.com>
2018-04-10 18:26:00 -0700

    updated firebase.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,41 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      data: null
+      data: null,
+      newData: ''
     };
+
+    this.dataRef = null;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  // when component mount trigger
   componentDidMount(){
     console.log('I DID MOUNT!');
-    database.ref().on('value', () =>{
-      console.log('THE DATA HANGED!');
+    this.dataRef = database.ref('dir1/dir2/dir3')
+
+    // using firebae function database check db node aka ref
+    database.ref().on('value', (snapshot) =>{
+      this.setState({
+        data: snapshot.val()
+      })
+      console.log('THE DATA HANGED!', snapshot.val());
     });
+  }
+
+
+  // on change store data to newData var
+  handleChange(event){
+    const newData = event.target.value;
+    this.setState({
+      newData
+    });
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    this.dataRef.push(this.state.newData);
   }
 
   render() {
@@ -26,8 +52,13 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          { JSON.stringify(this.state.data,null,2)}
         </p>
+
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.newData} onChange={this.handleChange} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
